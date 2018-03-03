02ccdef1bc32863b73bba129402ea0bf4847a089
Alexander <30376452+doograssen@users.noreply.github.com>
2018-03-04 00:13:25 +0300

    module6-task2

--- js/backend.js
+++ js/backend.js
@@ -1,9 +1,8 @@
 'use strict';
 
 (function () {
-  var SEND_URL = 'https://js.dump.academy/kekstagram';
-  var DATA_URL = 'https://js.dump.academy/kekstagram/data';
-  var TIME_OUT = 10000;
+  var sendURL = 'https://js.dump.academy/kekstagram';
+  var dataURL = 'https://js.dump.academy/kekstagram/data';
   /* --------------функция  с общими данными для создания запроса --------------*/
   var setupXHR = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
@@ -23,23 +22,23 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.timeout = TIME_OUT; // 10s
+    xhr.timeout = 10000; // 10s
     return xhr;
   };
 
   window.backend = {
     load: function (onLoad, onError) { // Загрузка данных
       var xhr = setupXHR(onLoad, onError);
-      xhr.open('GET', DATA_URL);
+      xhr.open('GET', dataURL);
       xhr.send();
     },
     save: function (data, onLoad, onError) { // отпрвка данных
       var xhr = setupXHR(onLoad, onError);
 
-      xhr.open('POST', SEND_URL);
+      xhr.open('POST', sendURL);
       xhr.send(data);
     },
-    showServerError: function (errorMessage) { // функция обратного вызова возникновения ошибки  при выполнении запроса
+    serverError: function (errorMessage) { // функция обратного вызова возникновения ошибки  при выполнении запроса
       var errorMessageBlock = document.createElement('div');
       var errorText = document.createElement('p');
       errorMessageBlock.appendChild(errorText);
--- js/picture.js
+++ js/picture.js
@@ -1,19 +1,19 @@
 'use strict';
 
 (function () {
-  var pictureTemplate = document.querySelector('#picture-template').content;
+  var templateElement = document.querySelector('#picture-template').content;
   /* ---------- Функция заполнения шаблона ----------------------------*/
   function fillTemplate(obj) {
-    var picture = pictureTemplate.cloneNode(true);
-    picture.querySelector('img').src = obj.url;
-    picture.querySelector('.picture-likes').textContent = obj.likes;
-    picture.querySelector('.picture-comments').textContent = obj.comments.length;
-    return picture;
+    var pictureElement = templateElement.cloneNode(true);
+    pictureElement.querySelector('img').src = obj.url;
+    pictureElement.querySelector('.picture-likes').textContent = obj.likes;
+    pictureElement.querySelector('.picture-comments').textContent = obj.comments.length;
+    return pictureElement;
   }
 
   /* ---------- Функция заполнения страницы фотографиями --------------*/
   window.picture = {
-    getPicturesFragment: function (arr) {
+    fillFragment: function (arr) {
       var fragment = document.createDocumentFragment();
       for (var i = 0; i < arr.length; i++) {
         fragment.appendChild(fillTemplate(arr[i]));
